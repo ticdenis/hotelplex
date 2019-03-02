19325f8d98ed1b4cfeb218c519bac6b582360b9d
Denis <denis@onnergy.com>
2019-03-02 15:26:24 +0100

    fix: missing hotelfixture dependency

--- src/AppBundle/DataFixtures/HotelFixture.php
+++ src/AppBundle/DataFixtures/HotelFixture.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\DataFixtures;
 
 use Doctrine\DBAL\Connection;
-use HotelPlex\Infrastructure\Presenter\ArrayHotelPresenter;
+use HotelPlex\Infrastructure\Presenter\Hotel\ArrayHotelPresenter;
 use HotelPlex\Tests\Infrastructure\Domain\Factory\FakerHotelFactory;
 
 class HotelFixture
@@ -21,8 +21,8 @@ class HotelFixture
         $values = $presenter->write($hotel)->read();
         unset($values['paymentMethods']);
         unset($values['images']);
-        $values['created_at'] = $hotel->createdAt()->value()->format('Y-m-d H:i:s');
-        $values['updated_at'] = $hotel->updatedAt()->value()->format('Y-m-d H:i:s');
+        $values['created_at'] = date('Y-m-d H:i:s');
+        $values['updated_at'] = date('Y-m-d H:i:s');
 
         $queryBuilder = $connection->createQueryBuilder()->insert('hotels');
         $index = 0;
